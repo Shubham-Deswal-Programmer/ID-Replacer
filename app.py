ddcb365515872a46ecd938956997d10ae031329e
Shubham Deswal <133574933+Shubham-Deswal-Programmer@users.noreply.github.com>
2025-05-03 17:31:52 +0530

    The final Show

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def replace_text_in_pdf(pdf_bytes, replacements):
             page.apply_redactions()
             for inst in text_instances:
                 # Apply 4-pixel vertical correction
-                new_position = fitz.Point(inst.tl.x, inst.tl.y + 10)
+                new_position = fitz.Point(inst.tl.x, inst.tl.y + 8)
                 page.insert_text(new_position, new, fontsize=10, fontname="Courier", color=(0, 0, 0))
     output = io.BytesIO()
     doc.save(output)
